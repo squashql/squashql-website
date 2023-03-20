3eeced18e9b9ea6e372427c6cff8ed29108cf63a
Arthur <nespoulous.arthur@yahoo.fr>
2023-03-20 12:20:32 +0100

    [feat] first website's version

---
+++ README.md
@@ -0,0 +1,8 @@
+# SquashQL
+
+## Requirements
+- Install [Jekyll](https://jekyllrb.com/docs/step-by-step/01-setup/)
+
+### Todo:
+- Add Analytics (in _layouts/default.html && assets/js/cookie.js)
+- Add all links (Discord / Github / use it now / contact / readme / contributing guidelines / Code of conduct)
---
+++ assets/js/cookie.js
@@ -0,0 +1,64 @@
+let accept = document.querySelector('#accept');
+let reject = document.querySelector('#reject');
+let container = document.querySelector('#cookie-container');
+
+// Check if user has given consent before.
+if (getCookie('cookie-consent') === null) {
+  // Display the cookie banner.
+  container.style.display = "flex";
+} else if (getCookie('cookie-consent') === "accepted") {
+  // The user already agreed to cookies.
+  initAnalytics();
+} else {
+  // The user already rejected cookies.
+}
+
+accept.addEventListener('click', function() {
+  setCookie('cookie-consent', 'accepted', 365);   // Add cookie as accepted.
+  initAnalytics();                                // Start using your analytics.
+  container.style.display = "none";               // Close the banner.
+});
+reject.addEventListener('click', function() {
+  deleteAllCookie();                              // Delete all cookies.
+  setCookie('cookie-consent', 'rejected', 365);   // Add cookie as rejected.
+  container.style.display = "none";               // Close the banner.
+});
+
+/**
+ * 
+ * @param {string} name 
+ * @returns {string|null} 
+ */
+function getCookie(name) {
+  const cookies = document.cookie.split('; ');
+  const value = cookies
+    .find(c => c.startsWith(name))
+    ?.split('=')[1]
+  if (value === undefined) {
+    return null
+  }
+  return decodeURIComponent(value);
+}
+
+/**
+ * 
+ * @param {string} name 
+ * @param {string} value 
+ * @param {number} days 
+ */
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setDate(date.getDate() + days);
+  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${date.toUTCString()};`;
+}
+
+function deleteAllCookie() {
+  document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+}
+
+function initAnalytics() {
+  // TODO: setup your own analytics.
+  // EXAMPLE:
+  // gtag('js', new Date());
+  // gtag('config', 'G-XXXXXXXX', { 'send_page_view': false });
+}
